5bb67d4ae1817d72ed41c7f96a319e9cf5805f86
sayan-acharyya <acharyyas735@gmail.com>
2026-09-25 01:18:59 +0530

    db conneection done

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDb } from "./configs/db.js";
 
 dotenv.config();
 
@@ -13,4 +14,5 @@ app.get("/",(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Auth-Service Started on ${PORT}`); 
+    connectDb();   
 })
